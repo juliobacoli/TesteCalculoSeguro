21afd3ec1422e4e7f88f064c0996efea007812df
Julio Bacoli <julio-microlins@hotmail.com>
2023-11-09 01:16:28 -0300

    Remover chamada swagger somente para ambiente de dev

--- TesteCalculoSeguro.API/Program.cs
+++ TesteCalculoSeguro.API/Program.cs
@@ -38,11 +38,8 @@ builder.Services.AddScoped<ISeguroRepository, SeguroRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
